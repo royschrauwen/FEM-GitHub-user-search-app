2cc3b5e1f1583f14466225370fd6d170ed84b825
Roy Schrauwen <roy@royschrauwen.nl>
2022-02-20 17:06:01 +0100

    CSS Grid for profile information

--- script.js
+++ script.js
@@ -2,20 +2,27 @@ const searchButton = document.querySelector("#button-search");
 
 const userName = document.querySelector(".profile-name");
 
-const url = "https://api.github.com/users/octocat";
-
 searchButton.addEventListener("click", searchUser);
 
-function searchUser() {
-  let user = document.querySelector("#input-searchbar");
-  console.log(user.value);
+let user = "octocat";
 
-  fetch(url)
+function searchUser() {
+  user = document.querySelector("#input-searchbar");
+
+  let defaultURL = "https://api.github.com/users/";
+  const urlToFetch = defaultURL.concat(user.value);
+
+  fetch(urlToFetch)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
+      console.log(data);
+
       userName.textContent = data.name;
+
+      document.querySelector(".profile-image").innerHTML =
+        "<img src=" + data.avatar_url + ">";
 
       document.querySelector("#counter-repos").textContent = data.public_repos;
       document.querySelector("#counter-followers").textContent = data.followers;
@@ -23,6 +30,13 @@ function searchUser() {
       document.querySelector(".profile-username").textContent =
         "@" + data.login;
       document.querySelector(".profile-date").textContent = data.created_at;
+      document.querySelector(".profile-bio").textContent = data.bio;
+      document.querySelector(".profile-bio").textContent =
+        "This profile has no bio";
+      document.querySelector("#location").textContent = data.location;
+      document.querySelector("#website").textContent = data.html_url;
+      document.querySelector("#twitter").textContent = data.twitter_username;
+      document.querySelector("#company").textContent = data.company;
     })
     .catch(() => {
       console.error("Problem with getting data from API");
